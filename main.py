73790978a835a3586890ba9aca0ea658f4fee8e6
iamjunhyeong <dddodo447@gmail.com>
2024-10-01 16:04:39 +0900

    addition

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ if b == '+':
 	print(a + c)
 elif b == '-':
 	print(a - c)
+elif b == '*':
+	print(a * c)
+elif b == '/':
+	if a == 0 or c == 0:
+		print("zero division")
+	print(a / c)
+else:
+	print("invalid")
